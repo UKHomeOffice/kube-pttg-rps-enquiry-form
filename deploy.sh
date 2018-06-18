06d872f6609077a2c7ac5c16208796b766769645
Stuart Craigen <s.craigen@mac.com>
2018-06-18 15:23:14 +0100

    Updated to replicate IP-FM-UI template

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,18 @@ if [[ -z ${VERSION} ]] ; then
     export VERSION=${IMAGE_VERSION}
 fi
 
-
-echo "deploy ${VERSION} to tools namespace, using PTTG_TOOLS drone secret"
-export KUBE_TOKEN=${PTTG_TOOLS}
-
+if [[ ${ENVIRONMENT} == "pr" ]] ; then
+    echo "deploy ${VERSION} to pr namespace, using PTTG_RPS_PR drone secret"
+    export KUBE_TOKEN=${PTTG_RPS_PR}
+else
+    if [[ ${ENVIRONMENT} == "test" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using PTTG_RPS_TEST drone secret"
+        export KUBE_TOKEN=${PTTG_RPS_TEST}
+    else
+        echo "deploy ${VERSION} to dev namespace, using PTTG_RPS_DEV drone secret"
+        export KUBE_TOKEN=${PTTG_RPS_DEV}
+    fi
+fi
 
 if [[ -z ${KUBE_TOKEN} ]] ; then
     echo "Failed to find a value for KUBE_TOKEN - exiting"
@@ -19,11 +27,15 @@ fi
 
 export WHITELIST=${WHITELIST:-0.0.0.0/0}
 
-export DNS_PREFIX=tools.notprod.
-export KC_REALM=pttg-qa
+if [ "${ENVIRONMENT}" == "pr" ] ; then
+    export DNS_PREFIX=
+    export KC_REALM=pttg-production
+else
+    export DNS_PREFIX=${ENVIRONMENT}.notprod.
+    export KC_REALM=pttg-qa
+fi
 
-
-export DOMAIN_NAME=rps-enquiry.${DNS_PREFIX}pttg.homeoffice.gov.uk
+export DOMAIN_NAME=rps-enqiry.${DNS_PREFIX}pttg.homeoffice.gov.uk
 
 echo "DOMAIN_NAME is $DOMAIN_NAME"
 
