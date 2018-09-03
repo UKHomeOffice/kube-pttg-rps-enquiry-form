d85a1a63d5d3f6205c6d3c06c6cc77c91e5616b9
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2018-09-03 17:24:37 +0100

    Download CA, verify token length

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,9 @@ if [[ ${ENVIRONMENT} == "pr" ]] ; then
     log "--- PRODUCTION PRODUCTION PRODUCTION"
     log "--- deploying ${VERSION} to pr namespace, using PTTG_RPS_PR drone secret"
     export KUBE_TOKEN=${PTTG_RPS_PR}
+    export CA_URL="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-prod.crt"
 else
+    export CA_URL="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
     if [[ ${ENVIRONMENT} == "test" ]] ; then
         log "--- deploying ${VERSION} to test namespace, using PTTG_RPS_TEST drone secret"
         export KUBE_TOKEN=${PTTG_RPS_TEST}
@@ -33,8 +35,18 @@ else
 fi
 
 if [[ -z ${KUBE_TOKEN} ]] ; then
-    echo "Failed to find a value for KUBE_TOKEN - exiting"
-    exit -1
+    log "[error] Failed to find a value for KUBE_TOKEN - exiting"
+    exit 78
+elif [ ${#KUBE_TOKEN} -ne 36 ] ; then
+    log "[error] Kubernetes token wrong length (expected 36, got ${#KUBE_TOKEN})"
+    exit 78
+fi
+
+log "--- downloading certificate authority for Kubernetes API"
+export KUBE_CERTIFICATE_AUTHORITY=/tmp/cert.crt
+if ! curl --silent --fail --retry 5 $CA_URL -o $KUBE_CERTIFICATE_AUTHORITY; then
+    log "[error] faled to download certificate authority!"
+    exit 1
 fi
 
 export WHITELIST=${WHITELIST:-0.0.0.0/0}
